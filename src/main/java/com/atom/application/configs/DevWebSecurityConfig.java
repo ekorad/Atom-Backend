5ec81032e369f7d95ff884ec0c4c78f9daefbd49
Vlad Zahiu <vladzahiu28@gmail.com>
2021-01-27 15:17:43 +0200

    Added complete JWT authentication functionality

--- src/main/java/com/atom/application/configs/DevWebSecurityConfig.java
+++ src/main/java/com/atom/application/configs/DevWebSecurityConfig.java
@@ -1,14 +1,25 @@
 package com.atom.application.configs;
 
+import com.atom.application.filters.JWTAuthorizationFilter;
+import com.atom.application.handlers.UsernamePasswordAuthenticationSuccessHandler;
 import com.atom.application.others.Http401UnauthorizedEntryPoint;
+import com.atom.application.services.WebUserDetailsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Profile("dev")
 @EnableWebSecurity(debug = true)
@@ -17,6 +28,39 @@ public class DevWebSecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Autowired
     private Http401UnauthorizedEntryPoint unauthorizedEntryPoint;
+    @Autowired
+    private UsernamePasswordAuthenticationSuccessHandler authSuccessHandler;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new WebUserDetailsService();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService());
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public JWTAuthorizationFilter jwtAuthorizationFilter() {
+        return new JWTAuthorizationFilter();
+    }
+
+    @Bean
+    public UsernamePasswordAuthenticationFilter usernamePasswordAuthenticationFilter() throws Exception {
+        UsernamePasswordAuthenticationFilter filter = new UsernamePasswordAuthenticationFilter(authenticationManager());
+        filter.setRequiresAuthenticationRequestMatcher(new AntPathRequestMatcher("/users/authenticate", "POST"));
+        filter.setAuthenticationSuccessHandler(authSuccessHandler);
+        return filter;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -33,7 +77,10 @@ public class DevWebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
             .and()
             .exceptionHandling()
-                .authenticationEntryPoint(unauthorizedEntryPoint);
+                .authenticationEntryPoint(unauthorizedEntryPoint)
+            .and()
+                .addFilterAfter(usernamePasswordAuthenticationFilter(), LogoutFilter.class)
+                .addFilterAfter(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
 }
